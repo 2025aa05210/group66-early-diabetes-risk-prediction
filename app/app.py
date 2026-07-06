09ee41604a1b5033f8b97c04466aa61ff12f5889
2025aa05210 <2025aa05210@wilp.bits-pilani.ac.in>
2026-07-06 17:31:18 +0530

    updated path of models

--- app/app.py
+++ app/app.py
@@ -18,8 +18,8 @@ st.set_page_config(
 # ----------------------------------
 
 try:
-    model = joblib.load("diabetes_model.pkl")
-    scaler = joblib.load("scaler.pkl")
+    model = joblib.load("/model/diabetes_model.pkl")
+    scaler = joblib.load("/model/scaler.pkl")
 except Exception as e:
     st.error(f"Error loading model files: {e}")
     st.stop()
