670fa49828631a880cc990790d26f7ee36941185
2025aa05210 <2025aa05210@wilp.bits-pilani.ac.in>
2026-08-04 13:28:03 +0530

    Add logger configuration for diabetes prediction

---
+++ src/utils/logger.py
@@ -0,0 +1,19 @@
+import logging
+from src.config import LOG_FILE
+
+logger = logging.getLogger("diabetes_prediction")
+
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter(
+    "%(asctime)s | %(levelname)s | %(filename)s | %(message)s"
+)
+
+file_handler = logging.FileHandler(LOG_FILE)
+file_handler.setFormatter(formatter)
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
